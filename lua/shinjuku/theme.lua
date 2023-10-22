d73f49314317568c40bc86cb7d7f5235531ee609
Samir Roy <rsamir@gmail.com>
2023-10-22 07:57:21 +0530

    v0.1.4

--- lua/shinjuku/theme.lua
+++ lua/shinjuku/theme.lua
@@ -399,7 +399,7 @@ theme.loadLSP = function()
 		LspDiagnosticsVirtualTextError = { fg = shinjuku.shinjuku11_gui }, -- Virtual text "Error"
 		LspDiagnosticsUnderlineError = { style = "undercurl", sp = shinjuku.shinjuku11_gui }, -- used to underline "Error" diagnostics.
 		LspDiagnosticsDefaultWarning = { fg = shinjuku.shinjuku15_gui }, -- used for "Warning" diagnostic signs in sign column
-		LspDiagnosticsSignWarning = { fg=shinjuku.shinjuku0_gui, bg = shinjuku.shinjuku15_gui }, -- used for "Warning" diagnostic signs in sign column
+		LspDiagnosticsSignWarning = { fg = shinjuku.shinjuku15_gui }, -- used for "Warning" diagnostic signs in sign column
 		LspDiagnosticsFloatingWarning = { fg = shinjuku.shinjuku15_gui }, -- used for "Warning" diagnostic messages in the diagnostics float
 		LspDiagnosticsVirtualTextWarning = { fg = shinjuku.shinjuku15_gui }, -- Virtual text "Warning"
 		LspDiagnosticsUnderlineWarning = { style = "undercurl", sp = shinjuku.shinjuku15_gui }, -- used to underline "Warning" diagnostics.
