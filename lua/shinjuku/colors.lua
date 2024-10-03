e09ea5d27699ee646a55e72064d29dbd3cfd6de5
Samir Roy <rsamir@gmail.com>
2024-10-03 20:08:28 +0530

    v1.0.1  - Add color for `@tag.tsx`  - Fix indents

--- lua/shinjuku/colors.lua
+++ lua/shinjuku/colors.lua
@@ -1,60 +1,62 @@
 local palette = require("shinjuku.palette")
 
 local colors = {
-	background = palette.black,
-	dark_background = palette.dark_gray,
-	light_background = palette.gray,
+  background = palette.black,
+  dark_background = palette.dark_gray,
+  light_background = palette.gray,
 
-	inactive_text = palette.light_gray,
-	dark_text = palette.darker_white,
-	darkest_text = palette.darkest_white,
+  inactive_text = palette.light_gray,
+  dark_text = palette.darker_white,
+  darkest_text = palette.darkest_white,
 
-	comment = palette.gray,
-	string = palette.red,
-	constant = palette.yellow,
+  comment = palette.gray,
+  string = palette.red,
+  constant = palette.yellow,
   bracket = palette.silver,
 
-	success = palette.green,
-	warning = palette.neon_yellow,
+  success = palette.green,
+  warning = palette.neon_yellow,
   error = palette.deep_pink,
 
-	substitute = palette.orange,
+  substitute = palette.orange,
   highlight = palette.blue,
   darkest = palette.darkest_gray,
   black = palette.pure_black,
   special = palette.deep_lavender,
-	none = palette.none,
+  none = palette.none,
 }
 
 -- Optional fewer colors in syntax highlighting
 if vim.g.shinjuku_colorful_syntax then
-	colors.syntax_default = palette.white
-	colors.syntax_function = palette.light_cyan
-	colors.syntax_keyword = palette.misty_rose
-	colors.syntax_param = palette.sea_green
+  colors.syntax_default = palette.white
+  colors.syntax_function = palette.light_cyan
+  colors.syntax_keyword = palette.misty_rose
+  colors.syntax_param = palette.sea_green
+  colors.syntax_tag = palette.light_cyan
 else
-	colors.syntax_default = palette.white
-	colors.syntax_function = palette.white
-	colors.syntax_keyword = palette.white
-	colors.syntax_param = palette.white
+  colors.syntax_default = palette.white
+  colors.syntax_function = palette.white
+  colors.syntax_keyword = palette.white
+  colors.syntax_param = palette.white
+  colors.syntax_tag = palette.white
 end
 
 -- Optional contrast sidebars, floating windows and popup menus
 if vim.g.shinjuku_contrast then
-	colors.sidebar = colors.dark_background
-	colors.float = colors.dark_background
+  colors.sidebar = colors.dark_background
+  colors.float = colors.dark_background
 else
-	colors.sidebar = colors.background
-	colors.float = colors.background
+  colors.sidebar = colors.background
+  colors.float = colors.background
 end
 
 -- Optional darker background for cursor line
 if vim.g.shinjuku_cursorline_transparent then
-	colors.cursorlinebg = palette.none
-	colors.cursorlinefg = palette.none
+  colors.cursorlinebg = palette.none
+  colors.cursorlinefg = palette.none
 else
-	colors.cursorlinebg = palette.darker_than_black
-	colors.cursorlinefg = palette.none
+  colors.cursorlinebg = palette.darker_than_black
+  colors.cursorlinefg = palette.none
 end
 
 return colors
--- lua/shinjuku/palette.lua
+++ lua/shinjuku/palette.lua
@@ -1,32 +1,32 @@
 local palette = {
   pure_black = "#000000",
   darker_than_black = "#121212",
-	black = "#1c1c1c",
+  black = "#1c1c1c",
   darkest_gray = '#262626',
-	dark_gray = "#303030",
-	gray = "#444444",
-	light_gray = "#585858",
+  dark_gray = "#303030",
+  gray = "#444444",
+  light_gray = "#585858",
   silver = "#808080",
-	darkest_white = "#bcbcbc",
-	darker_white = "#c6c6c6",
-	white = "#d0d0d0",
+  darkest_white = "#bcbcbc",
+  darker_white = "#c6c6c6",
+  white = "#d0d0d0",
 
-	blue = "#00d7ff",
-	red = "#ff99af",
-	orange = "#ff8700",
-	yellow = "#ffff87",
-	green = "#00c964",
+  blue = "#00d7ff",
+  red = "#ff99af",
+  orange = "#ff8700",
+  yellow = "#ffff87",
+  green = "#00c964",
 
   sky_blue = "#87afd7",
   sea_green = "#87d7af",
   misty_rose = "#d7afaf",
   light_cyan = "#afd7d7",
 
-	neon_yellow = "#ffff5f",
+  neon_yellow = "#ffff5f",
   deep_lavender = "#5f5f87",
   deep_pink = '#f54980',
 
-	none = "NONE",
+  none = "NONE",
 }
 
 return palette
